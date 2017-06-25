9ec609798a526a10d86a163a4da4a9937fbfa451
MaxwellGoblin <lispmachine@outlook.com>
2017-06-25 14:20:59 +0800

    [term]use get_terminal_size form std library

--- src/you_get/util/term.py
+++ src/you_get/util/term.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
+import shutil
+import sys
 
 def get_terminal_size():
     """Get (width, height) of the current terminal."""
+    if int(sys.version[2]) >= 3:
+        term_size = shutil.get_terminal_size((80, 40))
+        return term_size[1], term_size[0]
     try:
         import fcntl, termios, struct # fcntl module only available on Unix
         return struct.unpack('hh', fcntl.ioctl(1, termios.TIOCGWINSZ, '1234'))
-    except:
-        return (40, 80)
+    except Exception:
+#fallback here for windows and py version < 3.3. quite rare
+#getting terminal size from winapi GetConsoleScreenBufferInfo is easy
+#but I doubt it worths the effort
+        return (40, 80-10)
