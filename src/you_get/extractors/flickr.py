e921f024484492526c89783721f8592d21ab36d0
MaxwellGoblin <lispmachine@outlook.com>
2017-06-27 08:55:30 +0800

    [flickr]remove faker; slightly refactor

--- src/you_get/extractors/flickr.py
+++ src/you_get/extractors/flickr.py
@@ -5,16 +5,18 @@ __all__ = ['flickr_download']
 from ..common import *
 
 def flickr_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    page = get_html(url)
+    page = get_content(url)
     title = match1(page, r'<meta property="og:title" content="([^"]*)"')
     photo_id = match1(page, r'"id":"([0-9]+)"')
 
     try: # extract video
-        html = get_html('https://secure.flickr.com/apps/video/video_mtl_xml.gne?photo_id=%s' % photo_id)
+        html = get_content('https://secure.flickr.com/apps/video/video_mtl_xml.gne?photo_id=' +  photo_id)
         node_id = match1(html, r'<Item id="id">(.+)</Item>')
         secret = match1(html, r'<Item id="photo_secret">(.+)</Item>')
+        assert node_id
+        assert secret
 
-        html = get_html('https://secure.flickr.com/video_playlist.gne?node_id=%s&secret=%s' % (node_id, secret))
+        html = get_content('https://secure.flickr.com/video_playlist.gne?node_id=%s&secret=%s' % (node_id, secret))
         app = match1(html, r'APP="([^"]+)"')
         fullpath = unescape_html(match1(html, r'FULLPATH="([^"]+)"'))
         url = app + fullpath
@@ -23,12 +25,12 @@ def flickr_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
 
         print_info(site_info, title, mime, size)
         if not info_only:
-            download_urls([url], title, ext, size, output_dir, merge=merge, faker=True)
+            download_urls([url], title, ext, size, output_dir, merge=merge, headers=fake_headers)
 
-    except: # extract images
+    except Exception: # extract images
         image = match1(page, r'<meta property="og:image" content="([^"]*)')
         ext = 'jpg'
-        _, _, size = url_info(image)
+        size = url_size(image)
 
         print_info(site_info, title, ext, size)
         if not info_only:
