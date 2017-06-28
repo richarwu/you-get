611d939f4233ff01df5a17f281231f066f218e93
MaxwellGoblin <lispmachine@outlook.com>
2017-06-28 20:54:54 +0800

    [vimeo]do not change fake_headers

--- src/you_get/extractors/vimeo.py
+++ src/you_get/extractors/vimeo.py
@@ -28,19 +28,20 @@ def vimeo_download_by_channel_id(channel_id, output_dir='.', merge=False, info_o
         vimeo_download_by_id(id, None, output_dir, merge, info_only)
 
 def vimeo_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
+    headers = fake_headers.copy()
     try:
         # normal Vimeo video
         html = get_content('https://vimeo.com/' + id)
         config_url = unescape_html(r1(r'data-config-url="([^"]+)"', html))
-        video_page = get_content(config_url, headers=fake_headers)
+        video_page = get_content(config_url, headers=headers)
         title = r1(r'"title":"([^"]+)"', video_page)
         info = loads(video_page)
     except:
         # embedded player - referer may be required
         if 'referer' in kwargs:
-            fake_headers['Referer'] = kwargs['referer']
+            headers['Referer'] = kwargs['referer']
 
-        video_page = get_content('http://player.vimeo.com/video/%s' % id, headers=fake_headers)
+        video_page = get_content('http://player.vimeo.com/video/%s' % id, headers=headers)
         title = r1(r'<title>([^<]+)</title>', video_page)
         info = loads(match1(video_page, r'var t=(\{[^;]+\});'))
 
@@ -48,11 +49,11 @@ def vimeo_download_by_id(id, title=None, output_dir='.', merge=True, info_only=F
     streams = sorted(streams, key=lambda i: i['height'])
     url = streams[-1]['url']
 
-    type, ext, size = url_info(url, headers=fake_headers)
+    type, ext, size = url_info(url, headers=headers)
 
     print_info(site_info, title, type, size)
     if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge=merge, headers=fake_headers)
+        download_urls([url], title, ext, size, output_dir, merge=merge, headers=headers)
 
 def vimeo_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
     if re.match(r'https?://vimeo.com/channels/\w+', url):
